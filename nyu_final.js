ebfd6561fa80cbcb981683fc161b5903ca38b5c4
Bhuiyan, Yousuf (CORP) <Yousuf.Bhuiyan@adp.com>
2017-12-08 16:25:39 -0500

    added all my artwork folders

---
+++ nyu_final.js
@@ -0,0 +1,11 @@
+setTimeout (function(){
+	alert('Please check out ARTWORK section')
+},5000)
+
+function fn(){
+	console.log('hello!');
+}
+
+document.getElementById('clickme').onclick = function() {
+	alert('hi');
+});
